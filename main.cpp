b986edb8014c0e27c96d7832d68ab80fe972d494
SirDippingsauce <nobe5767@gmail.com>
2024-04-20 18:59:36 +0700

    intial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Henlo World" << '\n';    
+    std::cout << "Henlo World 2" << '\n';    
+    std::cout << "Henlo World 3" << '\n';    
+    std::cout << "Henlo World 4" << '\n';    
+    std::cout << "Henlo World 5" << '\n';    
+    std::cout << "Henlo World 5" << '\n';    
+}
